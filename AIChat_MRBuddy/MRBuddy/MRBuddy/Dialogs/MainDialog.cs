ab5185cd0f803f7afb2df94cc43c0589f7ad42c0
dineshnallapillai <71963626+dineshnallapillai@users.noreply.github.com>
2020-09-27 22:25:17 +0530

    Add files via upload

---
+++ AIChat_MRBuddy/MRBuddy/MRBuddy/Dialogs/MainDialog.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace MRBuddy
+{
+    public class MainDialog : ComponentDialog
+    {
+        private readonly UserState _userState;
+
+        public MainDialog(UserState userState)
+            : base(nameof(MainDialog))
+        {
+            _userState = userState;
+
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
+            {
+                InitialStepAsync,
+                FinalStepAsync,
+            }));
+
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private async Task<DialogTurnResult> InitialStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.EndDialogAsync(null, cancellationToken); //TO be removed 
+        }
+
+        private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+
+            return await stepContext.EndDialogAsync(null, cancellationToken);
+        }
+    }
+}
