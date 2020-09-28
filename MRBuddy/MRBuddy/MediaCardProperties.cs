5c06bfc698d332d74e4901634f55682bf61a54a2
Santhosh kumar K <santhosh.kumark@philips.com>
2020-09-28 10:32:09 +0530

    Added Media card

---
+++ MRBuddy/MRBuddy/MediaCardProperties.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MRBuddy
+{
+    public class MediaCardProperties
+    {
+        public String CardTitle { get; set; }
+        public String CardSubtitle { get; set; }
+        public String CardText { get; set; }
+        public String ImageURL{ get; set; }
+        public String MediaURL{ get; set; }
+        public String ButtonTitle{ get; set; }
+        public String Buttonvalue{ get; set; }
+
+    }
+}
